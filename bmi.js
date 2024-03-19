38e24643098e240ccf5b3cc1a6ba05bcbc63b3ed
tejeshvs <158245025+tejeshvs@users.noreply.github.com>
2024-03-19 22:46:14 +0530

    Delete bmi.js

--- bmi.js
+++
@@ -1,6 +0,0 @@
-function myfunc(){
-    let height=document.getElementById('height').value;
-    let weight=document.getElementById('weight').value;
-    let bmi= weight/(height*height);
-    document.getElementById('bmi').innerHTML= 'Your BMI is ' + bmi.toFixed(2);
-}
